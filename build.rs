0662d2d6574abcb1784681232a240dba3df7b756
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-09-21 15:43:13 -0700

    Bump to KaMinPar repo to recent

--- build.rs
+++ build.rs
@@ -6,21 +6,25 @@ fn main() {
         .define("KAMINPAR_64BIT_EDGE_WEIGHTS", "ON")
         .no_build_target(true)
         .build();
-    let path = format!("{}/build/library/", dst.display());
+    let path = format!("{}/build/kaminpar-shm/", dst.display());
+
+    println!("Building vendor KaMinPar finished successfully");
 
     cxx_build::bridge("src/lib.rs")
         .file("src/kaminpar_wrapper.cc")
-        .flag_if_supported("-std=c++17")
+        .flag_if_supported("-std=c++20")
         .define("KAMINPAR_64BIT_EDGE_IDS", "ON")
         .define("KAMINPAR_64BIT_EDGE_WEIGHTS", "ON")
         .static_flag(true)
         .compile("kaminpar-rs");
 
+    println!("Bridge compiled successfully");
+
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/kaminpar_wrapper.cc");
     println!("cargo:rerun-if-changed=include/kaminpar_wrapper.h");
     println!("cargo:rustc-link-search=native={}", path);
-    println!("cargo:rustc-link-lib=static=kaminpar");
+    println!("cargo:rustc-link-lib=static=kaminpar_shm");
     println!("cargo:rustc-link-lib=dylib=tbb");
     println!("cargo:rustc-link-lib=dylib=tbbmalloc");
 }
